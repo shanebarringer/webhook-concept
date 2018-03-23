e5204e34f06a3b3190e614661f3e956df7bccd04
Shane Barringer <shane.barringer@outlook.com>
2018-03-23 17:54:39 -0400

    removes call to mapAndPostIssues (temp)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const mapAndPostIssues = require("../issuesAndStories");
 
 router.post("/create-story", async (req, res) => {
   const { owner, name } = req.body;
-  mapAndPostIssues(owner, name).then(data => {
-    res.status(200).json({ message: "success" });
-  });
+
+  res.status(200).json({ message: "success" });
 });
 
 module.exports = router;
