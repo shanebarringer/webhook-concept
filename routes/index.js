3adfa9d2a4dd6f531817c3e16311636aa2e8ff5a
Shane Barringer <shane.barringer@outlook.com>
2018-03-23 21:06:05 -0400

    adds webhook for creating new issues and closing issues on GH and Pivotal Tracker

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 const router = require("express").Router();
+const issues = require("../issuesAndStories");
 
-router.post("/create-story", async (req, res) => {
-  const { owner, name } = req.body;
+router.post("/log-payload", (req, res) => {
+  const { issue, action } = req.body;
 
-  res.status(200).json({ message: "success" });
+  res.json({ title: "success" });
+});
+
+router.post("/create-story-from-issue", async (req, res) => {
+  const { issue, action } = req.body;
+
+  if (action === "opened") {
+    try {
+      const newStory = await issues.createStory(issue);
+      console.log(newStory);
+
+      const updatedIssue = await issues.updateIssueTitle(
+        issue.url,
+        issue.title,
+        newStory.id
+      );
+      console.log(updatedIssue);
+      res.status(200).json({ message: "success" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: err });
+    }
+  } else {
+    res.status(200).json({ message: "no action taken" });
+  }
+});
+
+router.post("/finish-story", async (req, res) => {
+  const { issue, action } = req.body;
+
+  if (action === "closed") {
+    try {
+      const title = issue.title.match(/\[[^\]]*\]/g)[0];
+      const storyID = title.substring(1, title.length - 1);
+      await issues.closeStory(storyID);
+      res.status(200).json({ message: "success" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: err });
+    }
+  } else {
+    res.status(200).json({ message: "no action taken" });
+  }
 });
 
 module.exports = router;
